0b542fb8f505d85db0b18e7b5a752082451ccfce
Hazze <hans.hazelius@gmail.com>
2014-08-30 13:13:00 +0200

    Use native JNIMessage
    Try to use native library, fallback
    to java messagelib.

--- demoservlet/src/main/java/se/hazelius/dev/demo/DemoServlet.java
+++ demoservlet/src/main/java/se/hazelius/dev/demo/DemoServlet.java
@@ -1,4 +1,5 @@
 package se.hazelius.dev.demo;
+
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -6,34 +7,40 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import se.hazelius.dev.demo.messagelib.MessageLib;
+import demolib.DemoLib;
+import se.hazelius.dev.demo.messagelib.MessageIF;
+import se.hazelius.dev.demo.messagelib.JavaMessage;
 
 public class DemoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1933123960656851478L;
-	private MessageLib mLib;
+	private MessageIF mLib;
 
 	@Override
 	public void init() throws ServletException {
-		mLib = new MessageLib();
+		try{
+			mLib = new DemoLib();
+		} catch (UnsatisfiedLinkError e){
+			mLib = new JavaMessage();
+		}
 		super.init();
 	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-     
 		req.getRequestDispatcher("jsp/index.jsp").forward(req, resp);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-	
-    String token = req.getParameter("token");
-    String message = mLib.getMessage();
-    req.setAttribute("message", message+" "+token);
 
-    req.getRequestDispatcher("jsp/index.jsp").forward(req, resp);
+		String token = req.getParameter("token");
+		long l = 0;
+		String message = mLib.getMessage(l);
+		req.setAttribute("message", message+" "+token);
+
+		req.getRequestDispatcher("jsp/index.jsp").forward(req, resp);
 
 
 	}
